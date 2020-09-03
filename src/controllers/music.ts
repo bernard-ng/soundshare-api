fd65a57cae9d2b6ce66b7003906c539b3e6a31a2
bernard-ng <ngandubernard@gmail.com>
2020-09-03 02:11:36 +0200

    app(api): lint fix

--- src/controllers/music.ts
+++ src/controllers/music.ts
@@ -2,42 +2,42 @@ import path from 'path'
 import fs from 'graceful-fs'
 import { Request, Response } from 'express'
 
-interface files {
-    name: string,
-    stats?: string,
-    hash?: string,
-    children: Array<files>
+interface HashedFile {
+  name: string,
+  stats?: string,
+  hash?: string,
+  children: Array<HashedFile>
 }
 
 const database = path.resolve(process.cwd(), 'database.json')
 
 export const index = async (request: Request, response: Response) => {
-    fs.promises.readFile(database, 'utf8')
-        .then(data => response.json(JSON.parse(data)))
-        .catch(error => {
-            console.error({ error })
-            return response.json({ error: "error while reading database" })
-        })
+  fs.promises.readFile(database, 'utf8')
+    .then(data => response.json(JSON.parse(data)))
+    .catch(error => {
+      console.error({ error })
+      return response.json({ error: 'error while reading database' })
+    })
 }
 
 export const show = async (request: Request, response: Response) => {
-    const rows = await fs.promises.readFile(database, 'utf8')
-    const files = JSON.parse(rows) as files
-    const directory = files.children.find(d => d.name === request.params.directory.toLowerCase())
+  const rows = await fs.promises.readFile(database, 'utf8')
+  const files = JSON.parse(rows) as HashedFile
+  const directory = files.children.find(d => d.name === request.params.directory.toLowerCase())
 
-    if (directory) {
-        const hash = request.params.hash
-        if (hash) {
-            const file = directory?.children.find(f => f.hash === hash);
+  if (directory) {
+    const hash = request.params.hash
+    if (hash) {
+      const file = directory?.children.find(f => f.hash === hash)
 
-            if (file) {
-                const path = `${process.env.APP_FILES_PATH}/${directory.name}/${file.name}`
-                return response.json({ path })
-            }
-            return response.status(404).json({ error: 'file not found !' });
-        }
-
-        return response.json(directory)
+      if (file) {
+        const path = `${process.env.APP_FILES_PATH}/${directory.name}/${file.name}`
+        return response.json({ path })
+      }
+      return response.status(404).json({ error: 'file not found !' })
     }
-    return response.status(404).json({ error: 'directory not found !' });
+
+    return response.json(directory)
+  }
+  return response.status(404).json({ error: 'directory not found !' })
 }
